e4f24cac22270c3b61d098abda7c5472a74ff43d
Tomáš Malík <maxeller.cz@gmail.com>
2020-07-21 14:58:39 +0200

    Added initial HTTPS configuration

--- README.md
+++ README.md
@@ -13,3 +13,6 @@
 - https://itnext.io/upgrade-an-asp-net-core-application-to-bootstrap-4-78cc2daf6450
 - https://docs.microsoft.com/en-us/aspnet/core/client-side/libman/libman-vs?view=aspnetcore-3.1
 - https://stackoverflow.com/questions/48481003/how-to-use-bootstrap-4-in-asp-net-core
+
+### HTTPS
+- https://docs.microsoft.com/cs-cz/aspnet/core/security/enforcing-ssl?view=aspnetcore-3.1&tabs=visual-studio
--- WebStorageSystem/Startup.cs
+++ WebStorageSystem/Startup.cs
@@ -35,7 +35,12 @@ namespace WebStorageSystem
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
             }
+
+            app.UseHttpsRedirection();
+
             app.UseStaticFiles();
 
             app.UseRouting();
