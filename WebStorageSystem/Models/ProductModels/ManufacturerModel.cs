4638847f8deca912c2951b8c4920fcd320e935b0
Tomáš Malík <maxeller.cz@gmail.com>
2020-10-01 14:14:02 +0200

    Model for last commit

---
+++ WebStorageSystem/Models/ProductModels/ManufacturerModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebStorageSystem.Data.Entities;
+
+namespace WebStorageSystem.Models.ProductModels
+{
+    public class ManufacturerModel : BaseEntityWithId
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        //public IEnumerable<Product> Products { get; set; }
+    }
+}
