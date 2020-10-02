88f02d78197271f8924df97ec0dd2581a0d4e4b7
Tomáš Malík <maxeller.cz@gmail.com>
2020-10-02 18:57:27 +0200

    Fix in site.js for delete/restore action in Areas

--- WebStorageSystem/wwwroot/js/site.js
+++ WebStorageSystem/wwwroot/js/site.js
@@ -7,7 +7,10 @@ $("#deleteRestoreModal").on("show.bs.modal", function(event) {
     const url = $(event.relatedTarget).data("url");
     const name = $(event.relatedTarget).data("name");
     const array = url.split("/");
-    const action = array[2];
+    let action = array[2];
+    if (!(action === "Delete" || action === "Restore")) {
+        action = array[3]; // For Actions in Areas
+    }
 
     $(this).find(".modal-header h5").text(action); // Changes action name (e.g. Delete/Restore) in header of modal window 
     $(this).find(".modal-body span").text(action.toLowerCase()); // Changes action name in body of modal window 
