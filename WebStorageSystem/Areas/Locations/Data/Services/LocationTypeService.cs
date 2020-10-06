f76f61c04ed9ebee8580540bdd10ba272746c267
Tomáš Malík <maxeller.cz@gmail.com>
2020-10-06 16:32:51 +0200

    Changes in LocationTypeService - Added xml comments - _getQuery is now readonly - _getQuery now uses Include to include Locations - GetLocationTypeAsync changed based on changes in _getQuery - DeleteLocationTypeAsync now counts only not softdeleted entires

--- WebStorageSystem/Areas/Locations/Data/Services/LocationTypeService.cs
+++ WebStorageSystem/Areas/Locations/Data/Services/LocationTypeService.cs
@@ -15,7 +15,7 @@ namespace WebStorageSystem.Areas.Locations.Data.Services
         private readonly LocationService _lService;
         private readonly ILogger _logger;
 
-        private IQueryable<LocationType> _getQuery;
+        private readonly IQueryable<LocationType> _getQuery;
 
         public LocationTypeService(AppDbContext context, LocationService lService, ILoggerFactory factory)
         {
@@ -27,38 +27,48 @@ namespace WebStorageSystem.Areas.Locations.Data.Services
             _getQuery = _context
                 .LocationTypes
                 .AsNoTracking()
-                .OrderBy(locationType => locationType.Name);
+                .OrderBy(locationType => locationType.Name)
+                .Include(locationType => locationType.Locations);
         }
 
+        /// <summary>
+        /// Gets entry from DB
+        /// </summary>
+        /// <param name="id">Entity ID</param>
+        /// <param name="getDeleted">Looks through soft deleted entries</param>
+        /// <returns>If found returns object, otherwise null</returns>
         public async Task<LocationType> GetLocationTypeAsync(int id, bool getDeleted = false)
         {
-            var locations = await _lService.GetLocationsAsync(getDeleted);
-            var myLocations = locations.ToList().FindAll(location => location.LocationType.Id == id);
-            LocationType locationType = null;
-            if (getDeleted)
-            {
-                locationType = await _getQuery.IgnoreQueryFilters().FirstOrDefaultAsync(locationType => locationType.Id == id);
-                locationType.Locations = myLocations;
-                return locationType;
-            }
-            
-            locationType = await _getQuery.FirstOrDefaultAsync(locationType => locationType.Id == id);
-            locationType.Locations = myLocations;
-            return locationType;
+            if (getDeleted) await _getQuery.FirstOrDefaultAsync(locationType => locationType.Id == id);
+            return await _getQuery.FirstOrDefaultAsync(locationType => locationType.Id == id);
         }
 
+        /// <summary>
+        /// Gets all entries from DB
+        /// </summary>
+        /// <param name="getDeleted">Looks through soft deleted entries</param>
+        /// <returns>Collection of entities</returns>
         public async Task<IEnumerable<LocationType>> GetLocationTypesAsync(bool getDeleted = false)
         {
             if (getDeleted) return await _getQuery.IgnoreQueryFilters().ToListAsync();
             return await _getQuery.ToListAsync();
         }
 
+        /// <summary>
+        /// Adds object to DB
+        /// </summary>
+        /// <param name="locationType">Object for adding</param>
         public async Task AddLocationTypeAsync(LocationType locationType)
         {
             _context.LocationTypes.Add(locationType);
             await _context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Edits entry in DB
+        /// </summary>
+        /// <param name="locationType">Object for editing</param>
+        /// <returns>Return tuple if editing was successful, if not error message is provided</returns>
         public async Task<(bool Success, string ErrorMessage)> EditLocationTypeAsync(LocationType locationType)
         {
             try
@@ -79,20 +89,34 @@ namespace WebStorageSystem.Areas.Locations.Data.Services
             }
         }
 
+        /// <summary>
+        /// Soft deletes entry based on entry ID
+        /// </summary>
+        /// <param name="id">Entry ID</param>
+        /// <returns>Number of affected rows (return -1 when entry is used as foreign key)</returns>
         public async Task<int> DeleteLocationTypeAsync(int id)
         {
             var locationType = await GetLocationTypeAsync(id);
             return await DeleteLocationTypeAsync(locationType);
         }
 
+        /// <summary>
+        /// Soft deletes entry based on object
+        /// </summary>
+        /// <param name="locationType">Object for deletion</param>
+        /// <returns>Number of affected rows (return -1 when entry is used as foreign key)</returns>
         public async Task<int> DeleteLocationTypeAsync(LocationType locationType)
         {
-            var lt = await GetLocationTypeAsync(locationType.Id);
-            if (lt.Locations.Count() != 0) return -1;
+            var n = locationType.Locations.Count(location => !location.IsDeleted);
+            if (n != 0) return -1;
             _context.LocationTypes.Remove(locationType);
             return await _context.SaveChangesAsync();
         }
 
+        /// /// <summary>
+        /// Restores soft deleted entry
+        /// </summary>
+        /// <param name="id">Entry ID</param>
         public async Task RestoreLocationTypeAsync(int id)
         {
             var locationType = await GetLocationTypeAsync(id, true);
@@ -101,6 +125,12 @@ namespace WebStorageSystem.Areas.Locations.Data.Services
             await _context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Determines existence of entry
+        /// </summary>
+        /// <param name="id">Entry ID</param>
+        /// <param name="getDeleted">Look through soft deleted entries</param>
+        /// <returns>True if entry exists</returns>
         public async Task<bool> LocationTypeExistsAsync(int id, bool getDeleted)
         {
             var locationType = await GetLocationTypeAsync(id, getDeleted);
