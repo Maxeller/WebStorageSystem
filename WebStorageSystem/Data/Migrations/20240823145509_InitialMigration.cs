386300f32901f3aa2015912b8e72b7cd45f3dfd3
Tomáš Malík <maxeller.cz@gmail.com>
2024-09-03 10:51:27 +0200

    Added creation of db view to Migration

--- WebStorageSystem/Data/Migrations/20240823145509_InitialMigration.cs
+++ WebStorageSystem/Data/Migrations/20240823145509_InitialMigration.cs
@@ -733,6 +733,13 @@ namespace WebStorageSystem.Data.Migrations
                 column: "NormalizedUserName",
                 unique: true,
                 filter: "[NormalizedUserName] IS NOT NULL");
+
+            migrationBuilder.Sql(@"CREATE VIEW [dbo].UnitBundleView
+	                                AS 
+		                            SELECT Units.InventoryNumber, Units.Id AS UnitId, NULL AS BundleId, Units.LocationId, Units.DefaultLocationId, Units.HasDefect, 'Unit' AS TableName FROM Units
+		                                WHERE Units.PartOfBundleId IS NULL
+	                                UNION ALL
+		                            SELECT Bundles.InventoryNumber, NULL AS UnitId, Bundles.Id AS BundleId, Bundles.LocationId, Bundles.DefaultLocationId, Bundles.HasDefect, 'Bundle' AS TableName FROM Bundles");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
